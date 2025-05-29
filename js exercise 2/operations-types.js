b0179b5a1616889d444b76f6592b7d86699e74e1
SMITH MOGAMAT ZUHAYR <126153069+kxngzero329@users.noreply.github.com>
2025-05-29 19:02:05 +0200

    Add files via upload

---
+++ js exercise 2/operations-types.js
@@ -0,0 +1,16 @@
+// TODO: Create a variable called number1 and store any number in it
+let number1 = 5;
+// TODO: Create a variable called number2 and store a different number in it
+let number2 = 10;
+// TODO: Convert number1 to a string and store it in a new variable
+let number1String = number1.toString();
+// TODO: Add number1 and number2 and store the result in a variable called sum
+let sum = number1 + number2;
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+let product = number1 * number2;
+// TODO: Print all results to the console
+console.log("number1:", number1);
+console.log("number2:", number2);
+console.log("number1 as string is ", number1String);
+console.log("sum is 5 + 10 = ", sum);
+console.log("product is 5 * 10 = ", product);
